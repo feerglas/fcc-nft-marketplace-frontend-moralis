358e94b350016f2f7171ce1e42ae8707a52091a5
Yves Tscherry <info@feerglas.com>
2022-06-09 12:52:47 +0200

    move head to app.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Head from 'next/head';
-
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="Buy and sell nfts" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      nft
-    </div>
-  );
+  return <div>nft</div>;
 }
