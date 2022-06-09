a11d8795f11db2bb5dbd20bb116e15462309a03f
Yves Tscherry <info@feerglas.com>
2022-06-09 14:24:03 +0200

    init moralis provider with app id andserver url

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import { MoralisProvider } from 'react-moralis';
 import Header from '../components/Header';
 import Head from 'next/head';
 
+const APP_ID = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
+
 function MyApp({ Component, pageProps }) {
   return (
     <div>
@@ -11,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Buy and sell nfts" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MoralisProvider initializeOnMount={false}>
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
         <Header />
         <Component {...pageProps} />
       </MoralisProvider>
