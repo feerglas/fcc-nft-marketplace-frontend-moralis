05406d829c7473ad0d3d077bab08430257a4fdf4
Yves Tscherry <info@feerglas.com>
2022-06-09 14:24:15 +0200

    add explanation comments

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+// How to get all listed NFTs? They are stored in the contract in a mapping...
+// -> We will index the events off-chain and then read from our database.
+// -> Setup a server to listen for those events to be fired, and add them to a
+// database to query
+
 export default function Home() {
   return <div>nft</div>;
 }
