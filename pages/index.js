1e8ca02183b638ac7e0ea68a8ecae60bb0011918
Yves Tscherry <info@feerglas.com>
2022-06-10 16:09:14 +0200

    add boxes to show nft listings

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,46 @@
 // -> Setup a server to listen for those events to be fired, and add them to a
 // database to query
 
+import { useMoralisQuery, useMoralis } from 'react-moralis';
+import NFTBox from '../components/NFTBox';
+
 export default function Home() {
-  return <div>nft</div>;
+  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+    'ActiveItem',
+    (query) => query.limit(10).descending('tokenId')
+  );
+
+  const { isWeb3Enabled } = useMoralis();
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="px-4 py-4 font-bold text-2xl">Recently Listed</h1>
+      <div className="flex flex-wrap">
+        {isWeb3Enabled ? (
+          fetchingListedNfts ? (
+            <div>Loading</div>
+          ) : (
+            listedNfts.map((nft, index) => {
+              const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                nft.attributes;
+
+              return (
+                <div key={index}>
+                  <NFTBox
+                    price={price}
+                    nftAddress={nftAddress}
+                    tokenId={tokenId}
+                    marketplaceAddress={marketplaceAddress}
+                    seller={seller}
+                  />
+                </div>
+              );
+            })
+          )
+        ) : (
+          <div>web3 currently not enabled</div>
+        )}
+      </div>
+    </div>
+  );
 }
