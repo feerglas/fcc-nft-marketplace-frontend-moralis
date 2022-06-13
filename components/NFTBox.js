0e94fab8c524b21e49c35069e11b3cf10c538827
Yves Tscherry <info@feerglas.com>
2022-06-13 09:43:07 +0200

    add functionality to buy item

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -3,7 +3,7 @@ import { useWeb3Contract, useMoralis } from 'react-moralis';
 import nftMarketplaceAbi from '../constants/NFTMarketplace.json';
 import nftAbi from '../constants/BasicNft.json';
 import Image from 'next/image';
-import { Card } from 'web3uikit';
+import { Card, useNotification } from 'web3uikit';
 import { ethers } from 'ethers';
 import UpdateListingModal from './UpdateListingModal';
 
@@ -40,11 +40,24 @@ export default function NFTBox({
 
   const hideModal = () => setShowModal(false);
 
+  const dispatch = useNotification();
+
   const { runContractFunction: getTokenURI } = useWeb3Contract({
     abi: nftAbi,
     contractAddress: nftAddress,
     functionName: 'tokenURI',
     params: {
+      tokenId,
+    },
+  });
+
+  const { runContractFunction: buyItem } = useWeb3Contract({
+    abi: nftMarketplaceAbi,
+    contractAddress: marketplaceAddress,
+    functionName: 'buyItem',
+    msgValue: price,
+    params: {
+      nftAddress,
       tokenId,
     },
   });
@@ -76,8 +89,22 @@ export default function NFTBox({
     ? 'you'
     : truncateString(seller || '', 15);
 
+  const handleBuyItemSuccess = () => {
+    dispatch({
+      type: 'success',
+      message: 'Item bought',
+      title: 'Item bought',
+      position: 'topR',
+    });
+  };
+
   const handleCardClick = () => {
-    isOwnedByUser ? setShowModal(true) : console.log('will init buy');
+    isOwnedByUser
+      ? setShowModal(true)
+      : buyItem({
+          onError: (err) => console.log(err),
+          onSuccess: handleBuyItemSuccess,
+        });
   };
 
   return (
